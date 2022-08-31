8a0a15b2fc3c4adf56c7e9e5b9fd5600c6bf005c
Chris Blanquera <chris@incept.asia>
2022-08-31 17:28:24 +0800

    leaderboards

--- docs/scripts/members.js
+++ docs/scripts/members.js
@@ -1,8 +1,15 @@
 window.addEventListener('web3sdk-ready', async _ => {
   //------------------------------------------------------------------//
   // Variables
+
+  const template = {
+    board: document.getElementById('template-leader-board').innerHTML,
+    row: document.getElementById('template-leader-row').innerHTML
+  }
+
   //------------------------------------------------------------------//
   // Functions 
+
   //------------------------------------------------------------------//
   // Events
 
@@ -14,4 +21,26 @@ window.addEventListener('web3sdk-ready', async _ => {
 
   //------------------------------------------------------------------//
   // Initialize
+
+  const response = await fetch('https://www.incept.asia/milk.php')
+  const leaders = (await response.json()).holders.map(row => {
+    row.balance = Web3SDK.toEther((new bigDecimal(row.balance)).getValue())
+    return row
+  })
+
+  const board = theme.toElement(template.board)
+
+  leaders.forEach(row => {
+    board.querySelector('tbody').appendChild(
+      theme.toElement(template.row
+        .replace('{ADDRESS}', `${row.address.substring(0, 4)}...${
+          row.address.substring(row.address.length - 4)
+        }`)
+        .replace('{BALANCE}', parseFloat(row.balance).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+        .replace('{PERCENT}', `${row.share}%`)
+      )
+    )
+  })
+
+  document.querySelector('section.section-1').appendChild(board)
 })
