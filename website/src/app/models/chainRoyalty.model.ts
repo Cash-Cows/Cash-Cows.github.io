35e91af88c70b90ad946deed7593d40fb740f0ae
Chris Blanquera <chris@incept.asia>
2022-09-11 21:06:31 +0800

    removing website

--- website/src/app/models/chainRoyalty.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ChainRoyaltyModel { 
-    TOTAL_SHARES?:number;
-    MINT_PRICE?:number;
-    MAX_PER_WALLET?:number;
-    MAX_FREE_PER_WALLET?:number;
-    minted?:number;
-    saleStarted?:boolean; 
-}
