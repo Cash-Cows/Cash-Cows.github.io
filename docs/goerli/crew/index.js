8b487dd55117aa3101faed60b2bef171c67788e2
Chris Blanquera <chris@incept.asia>
2022-11-10 03:43:34 +0800

    gallery to marketplace

--- docs/goerli/crew/index.js
+++ docs/goerli/crew/index.js
@@ -1,6 +1,189 @@
 window.addEventListener('web3sdk-ready', async _ => {
   //------------------------------------------------------------------//
   // Variables
+
+  const networkName = window.location.pathname.split('/')[1] || 'ethereum'
+  const network = Web3SDK.network(networkName)
+
+  const contract = {
+    crew: network.contract('crew'),
+    metadata: network.contract('metadata')
+  }
+
+  const template = {
+    row: document.getElementById('template-form-row').innerHTML,
+    fieldset: document.getElementById('template-form-fieldset').innerHTML,
+    item: document.getElementById('template-result-item').innerHTML
+  }
+
+  const element = {
+    results: document.querySelector('main.results'),
+    pagination: document.querySelector('a.pagination'),
+    options: document.querySelector('aside.filters div.attributes div.options')
+  }
+
+  let database = []
+  let sort = 'floorAskPrice'
+  let next = null
+  let limit = 50
+  let attributes = {}
+  
+  //------------------------------------------------------------------//
+  // Functions
+
+  const search = async _ => {
+    const params = new URLSearchParams()
+    params.set('collection', `0x.${contract.crew.address.substring(2)}`)
+    params.set('limit', String(limit))
+    params.set('sortBy', sort)
+
+    for (const trait in attributes) {
+      params.set(`attributes[${trait}]`, attributes[trait])
+    }
+    
+    if (next) {
+      params.set('continuation', next)
+    }
+    const response = await fetch(
+      `https://www.incept.asia/cashcows/reservoir/tokens/v5?${
+        decodeURIComponent(params.toString())
+      }`
+    )
+
+    const json = await response.json()
+
+    if (json.error) {
+      next = null
+      theme.hide(element.pagination, true)
+      return
+    }
+
+    for (const row of json.results.tokens) {
+      const tokenId = row.token.tokenId
+      const data = database.rows.filter(
+        data => data.edition == parseInt(tokenId)
+      )[0]
+
+      if (!data) continue
+
+      let badge = 'muted'
+      if (data.rank < 100) {
+        badge = 'success'
+      } else if (data.rank < 500) {
+        badge = 'warning'
+      } else if (data.rank < 1000) {
+        badge = 'info'
+      }
+
+      let price = ' '
+      if (row.market?.floorAsk?.price?.amount?.decimal) {
+        price = `${row.market.floorAsk.price.amount.decimal} ETH`
+      }
+      
+      const level = data.attributes.Level.value
+
+      const item = theme.toElement(template.item, {
+        '{PRICE}': price,
+        '{NAME}': `#${tokenId}`,
+        '{RANK}': data.rank,
+        '{BADGE}': badge,
+        '{SCORE}': data.score,
+        '{ID}': tokenId,
+        '{LEVEL}': level,
+        '{IMAGE}': `https://assets.wearecashcows.com/cashcows/crew/image/${tokenId}_${level - 1}.png`,
+        '{HREF}': `/${networkName}/crew/${tokenId}/profile.html`
+      })
+      element.results.appendChild(item)
+      window.doon(item)
+    }
+
+    if (json.results.continuation) {
+      next = json.results.continuation
+      theme.hide(element.pagination, false)
+    } else {
+      next = null
+      theme.hide(element.pagination, true)
+    }
+  }
+
+  const filters = () => {
+    //populate attribute filters
+    for (const attribute in database.occurances) {
+      const set = theme.toElement(template.fieldset, {'{LEGEND}': attribute})
+      const fields = set.querySelector('div.fields')
+      Object.keys(database.occurances[attribute])
+        .sort((a, b) => database.occurances[attribute][a] - database.occurances[attribute][b])
+        .forEach(trait => {
+          const row = theme.toElement(template.row, {
+            '{NAME}': `attribute[${attribute}]`,
+            '{VALUE}': trait,
+            '{LABEL}': trait,
+            '{COUNT}': database.occurances[attribute][trait]
+          })
+      
+          fields.appendChild(row)
+        })
+  
+      element.options.appendChild(set)
+      window.doon(set)
+    }
+  }
+
+
+  //------------------------------------------------------------------//
+  // Events
+
+  window.addEventListener('db-crew-ready', _ => {
+    database = Web3SDK.state.crew
+    filters()
+    search()
+  })
+
+  window.addEventListener('toggle-click', async e => {
+    e.for.classList.toggle('open')
+    if (e.for.classList.contains('open')) {
+      e.for.nextElementSibling.style.display = 'block'
+    } else {
+      e.for.nextElementSibling.style.display = 'none'
+    }
+  })
+
+  window.addEventListener('filter-click', async e => {
+    const name = e.for.name.replace('attribute[', '').replace(']', '')
+    if (attributes[name] == e.for.value) e.for.checked = false
+    attributes = {}
+    document.querySelectorAll('aside.filters input[type=radio]').forEach(input => {
+      if (!input.checked) return
+      const name = input.name.replace('attribute[', '').replace(']', '')
+      attributes[name] = input.value
+    })
+
+    next = null
+    element.results.innerHTML = ''
+    theme.hide(element.pagination, true)
+    search()
+  })
+
+  window.addEventListener('sort-change', async e => {
+    sort = e.for.value
+    next = null
+    element.results.innerHTML = ''
+    theme.hide(element.pagination, true)
+    search()
+  })
+
+  window.addEventListener('next-click', async e => {
+    theme.hide(element.pagination, true)
+    search()
+  })
+
+  //------------------------------------------------------------------//
+  // Initialize
+})
+
+window.addEventListener('web3sdk2-ready', async _ => {
+  //------------------------------------------------------------------//
+  // Variables
   const networkName = window.location.pathname.split('/')[1] || 'ethereum'
   const network = Web3SDK.network(networkName)
   const range = 24
